9152bf856d0fb51f0c7c47ac55e469f7c7880f8c
Andy Liu <andyliu@Andys-MacBook-Pro-2.local>
2018-05-08 11:48:10 -0400

    window, player, threshold

--- battle.py
+++ battle.py
@@ -1,4 +1,7 @@
 import pygame
+import random, os.path
+from pygame.locals import *
+
 import time
 import random
 
@@ -6,6 +9,13 @@ pygame.init()
 
 display_width = 800
 display_height = 600
+
+MAX_SHOTS      = 2      #most player bullets onscreen
+ALIEN_ODDS     = 22     #chances a new alien appears
+BOMB_ODDS      = 60    #chances a new bomb will drop
+ALIEN_RELOAD   = 12     #frames between new aliens
+SCREENRECT     = Rect(0, 0, 640, 480)
+SCORE          = 0
 
 
 gameDisplay = pygame.display.set_mode((display_width, display_height))
@@ -15,6 +25,63 @@ clock = pygame.time.Clock()
 
 
 black=(0,0,0)
+
+main_dir = os.path.split(os.path.abspath(__file__))[0]
+
+def load_image(file):
+    "loads an image, prepares it for play"
+    file = os.path.join(main_dir, 'data', file)
+    try:
+        surface = pygame.image.load(file)
+    except pygame.error:
+        raise SystemExit('Could not load image "%s" %s'%(file, pygame.get_error()))
+    return surface.convert()
+
+def load_images(*files):
+    imgs = []
+    for file in files:
+        imgs.append(load_image(file))
+    return imgs
+
+class Player(pygame.sprite.Sprite):
+    speed = 10
+    bounce = 24
+    gun_offset = -11
+    images = []
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self, self.containers)
+        self.image = self.images[0]
+        self.rect = self.image.get_rect(midbottom=SCREENRECT.midbottom)
+        self.reloading = 0
+        self.origtop = self.rect.top
+        self.facing = -1
+
+    def move(self, direction):
+        if direction: self.facing = direction
+        self.rect.move_ip(direction*self.speed, 0)
+        self.rect = self.rect.clamp(SCREENRECT)
+        if direction < 0:
+            self.image = self.images[0]
+        elif direction > 0:
+            self.image = self.images[1]
+        self.rect.top = self.origtop - (self.rect.left//self.bounce%2)
+
+
+class Score(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.font = pygame.font.Font(None, 20)
+        self.font.set_italic(1)
+        self.color = Color('white')
+        self.lastscore = -1
+        self.update()
+        self.rect = self.image.get_rect().move(10, 450)
+
+    def update(self):
+        if SCORE != self.lastscore:
+            self.lastscore = SCORE
+            msg = "Score: %d" % SCORE
+            self.image = self.font.render(msg, 0, self.color)
 
 
 def crash():
@@ -41,21 +108,34 @@ def game_intro():
 
         pygame.display.update()
         clock.tick(15)
+        intro = False
 
 
 def game_loop():
+    # Set the display mode
+    background = pygame.Surface(SCREENRECT.size)
+    winstyle = 0  # |FULLSCREEN
+    bestdepth = pygame.display.mode_ok(SCREENRECT.size, winstyle, 32)
+    screen = pygame.display.set_mode(SCREENRECT.size, winstyle, bestdepth)
+
     x = (display_width * 0.45)
     y = (display_height * 0.8)
-
     x_change = 0
+    img = load_image('plane.png')
+    Player.images = [img, pygame.transform.flip(img, 1, 0)]
+    all = pygame.sprite.RenderUpdates()
+    Player.containers = all
+    Score.containers = all
 
-
-
-
+    global score
+    kills = 0
+    clock = pygame.time.Clock()
+    global SCORE
+    player = Player()
 
     gameExit = False
 
-    while not gameExit:
+    while player.alive():
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -71,16 +151,26 @@ def game_loop():
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                     x_change = 0
+        keystate = pygame.key.get_pressed()
+        # clear/erase the last drawn sprites
+        all.clear(screen, background)
+
+        # update all the sprites
+        all.update()
+
+        # handle player input
+        direction = keystate[K_RIGHT] - keystate[K_LEFT]
+        player.move(direction)
 
         x += x_change
-        gameDisplay.fill(0,0,0)
+        gameDisplay.fill(black)
 
 
 
 
 
 
-game_intro()
+#game_intro()
 game_loop()
 pygame.quit()
 quit()
