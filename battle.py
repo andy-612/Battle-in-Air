ef858c4a844573b1fa2437a039dc5ccbdca7010f
Andy Liu <andyliu@Andys-MacBook-Pro-2.local>
2018-05-29 11:50:54 -0400

    rock images

--- battle.py
+++ battle.py
@@ -9,11 +9,12 @@ pygame.init()
 
 
 MAX_SHOTS      = 2      #most player bullets onscreen
-ALIEN_ODDS     = 22     #chances a new alien appears
+ROCK_ODDS     = 22     #chances a new alien appears
 BOMB_ODDS      = 60    #chances a new bomb will drop
-ALIEN_RELOAD   = 12     #frames between new aliens
+ROCK_RELOAD   = 12     #frames between new aliens
 SCREENRECT     = Rect(0, 0, 1280, 720)
 SCORE          = 0
+
 
 
 pygame.display.set_caption('Battle in Air')
@@ -22,6 +23,7 @@ clock = pygame.time.Clock()
 
 
 black=(0,0,0)
+DARKGREEN=(0,50,0)
 
 main_dir = os.path.split(os.path.abspath(__file__))[0]
 
@@ -71,6 +73,31 @@ class Player(pygame.sprite.Sprite):
     def update(self):
         self.rect.y = self.rect.y + self.movey
 
+class Rock(pygame.sprite.Sprite):
+    speed = 1
+    animcycle = 2
+    images = []
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self, self.containers)
+        self.image = self.images[0]
+        self.rect = self.image.get_rect()
+        self.facing = random.choice((-1,1)) * Rock.speed
+        self.frame = 0
+        if self.facing < 0:
+            self.rect.right = SCREENRECT.right
+
+    def update(self):
+        self.rect.move_ip(self.facing, 10)
+        if not SCREENRECT.contains(self.rect):
+            self.facing = -self.facing;
+            #self.rect.top = self.rect.bottom + 1
+            #self.rect = self.rect.clamp(SCREENRECT)
+        # self.frame = self.frame + 1
+        # self.image = self.images[self.frame//self.animcycle%3]
+
+
+
+
 
 class Score(pygame.sprite.Sprite):
     def __init__(self):
@@ -118,19 +145,24 @@ def game_intro():
 def game_loop():
     # Set the display mode
     background = pygame.Surface(SCREENRECT.size)
+    background.fill(DARKGREEN)
     winstyle = 0  # |FULLSCREEN
     bestdepth = pygame.display.mode_ok(SCREENRECT.size, winstyle, 32)
     screen = pygame.display.set_mode(SCREENRECT.size, winstyle, bestdepth)
 
-
+    rocks = pygame.sprite.Group()
     x_change = 0
     img = load_image('plane.png')
     Player.images = [img, pygame.transform.flip(img, 1, 0)]
+    img = load_image('blockade image.png')  
+    Rock.images= [img, pygame.transform.flip(img, 1, 0)]
     all = pygame.sprite.RenderUpdates()
     Player.containers = all
+    Rock.containers = rocks, all
     Score.containers = all
 
     global score
+    rockreload = ROCK_RELOAD
     kills = 0
     clock = pygame.time.Clock()
     global SCORE
@@ -161,6 +193,7 @@ def game_loop():
                     x_change = 0
         keystate = pygame.key.get_pressed()
         # clear/erase the last drawn sprites
+        screen.fill(DARKGREEN)
         all.clear(screen, background)
 
         # update all the sprites
@@ -175,7 +208,13 @@ def game_loop():
         player.update()
 
 
-        #drav the scene
+        # Create new rocks
+        if rockreload:
+            rockreload = rockreload - 1
+        elif not int(random.random() * ROCK_ODDS):
+            Rock()
+            rockreload = ROCK_RELOAD
+            #drav the scene
         dirty = all.draw(screen)
         pygame.display.update(dirty)
 
